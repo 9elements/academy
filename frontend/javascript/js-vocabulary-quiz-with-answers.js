6e60120433f9c15ee3a2e5d45d46eea33974f3d4
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2018-06-25 17:52:54 +0200

    Moa quiz

---
+++ frontend/javascript/js-vocabulary-quiz-with-answers.js
@@ -0,0 +1,26 @@
+# Variablen und Konstanten
+# Literale
+  - String
+  - Array
+  - Objekt
+  - Boolean
+# Funktionen / Methoden
+mit Phat-Arrow
+# Parameter
+# Rückgabewert
+# Objekte (Eigenschaften / Werte)
+
+## Advanced
+
+Import / Export
+
+Klassen
+Constructoren / super
+Vererbung mit super
+
+map
+  - API Response mit Buchstaben als Postion im Alphabet
+filter
+
+Promises
+  - mach mal ein Fetch auf diese API
